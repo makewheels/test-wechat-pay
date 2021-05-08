78d6a694eb396393ef8e9fe9bb9b89d60669c60c
makewheels <makewheels>
2021-05-08 23:04:45 +0800

    init

---
+++ src/main/java/com/example/testwechatpay/TestWechatPayApplication.java
@@ -0,0 +1,13 @@
+package com.example.testwechatpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestWechatPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestWechatPayApplication.class, args);
+    }
+
+}
