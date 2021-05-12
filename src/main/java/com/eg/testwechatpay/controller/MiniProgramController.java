35d371127fc139e0252fd3b110f282d004287303
makewheels <makewheels>
2021-05-12 23:49:06 +0800

    小程序登陆

--- src/main/java/com/eg/testwechatpay/controller/MiniProgramController.java
+++ src/main/java/com/eg/testwechatpay/controller/MiniProgramController.java
@@ -1,7 +1,9 @@
 package com.eg.testwechatpay.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.eg.testwechatpay.service.MiniProgramService;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -16,4 +18,14 @@ public class MiniProgramController {
     public String getQRCode() {
         return miniProgramService.getQRCode();
     }
+
+    @RequestMapping("login")
+    public String login(@RequestParam String js_code) {
+        String json = miniProgramService.login(js_code);
+        JSONObject jsonObject = JSONObject.parseObject(json);
+        JSONObject result = new JSONObject();
+        result.put("openid", jsonObject.getString("openid"));
+        return result.toJSONString();
+    }
+
 }
--- src/main/java/com/eg/testwechatpay/service/MiniProgramService.java
+++ src/main/java/com/eg/testwechatpay/service/MiniProgramService.java
@@ -4,8 +4,11 @@ import cn.hutool.http.HttpRequest;
 import cn.hutool.http.HttpResponse;
 import cn.hutool.http.HttpUtil;
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.eg.testwechatpay.bean.qrcode.QRCodeRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.SystemUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -15,6 +18,7 @@ import java.io.File;
 import java.io.IOException;
 
 @Service
+@Slf4j
 public class MiniProgramService {
     @Value("${wechat.appid}")
     private String appid;
@@ -26,12 +30,12 @@ public class MiniProgramService {
         String url = "https://api.weixin.qq.com/wxa/getwxacodeunlimit?access_token="
                 + secretService.getAccessToken();
         QRCodeRequest request = new QRCodeRequest();
-        request.setScene("v=fea3285322516");
+        request.setScene(RandomStringUtils.randomAlphanumeric(16));
         HttpResponse response = HttpRequest.post(url)
                 .body(JSON.toJSONString(request))
                 .execute();
-        File file = new File(SystemUtils.getUserHome(),
-                "Downloads/" + System.currentTimeMillis() + ".jpg");
+        File file = new File(SystemUtils.getUserHome()
+                + "/Downloads/" + System.currentTimeMillis() + ".jpg");
         try {
             FileUtils.writeByteArrayToFile(file, response.bodyBytes());
         } catch (IOException e) {
@@ -41,8 +45,12 @@ public class MiniProgramService {
     }
 
     public String login(String js_code) {
-        return HttpUtil.get("https://api.weixin.qq.com/sns/jscode2session?appid=" + appid
+        String json = HttpUtil.get("https://api.weixin.qq.com/sns/jscode2session?appid=" + appid
                 + "&secret=" + secretService.getAppSecret() + "&js_code=" + js_code
                 + "&grant_type=authorization_code");
+        JSONObject jsonObject = JSONObject.parseObject(json);
+        String openid = jsonObject.getString("openid");
+        log.info("小程序登陆, js_code = {}, openid = {}", js_code, openid);
+        return json;
     }
 }
